40cd12da930d6e7fc92c105e9f8cfc23cf97d9cc
Markotip <rafifn11@gmail.com>
2024-12-22 11:31:47 +0700

    penyesuaian css iframe materi & penambahan video materi pemula

--- materi_pemula.php
+++ materi_pemula.php
@@ -19,20 +19,25 @@ include 'koneksi.php';
                     <h1>Kata Ganti</h1>
                     <div class="vid-container">
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Aku.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Dia.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Kami.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Kamu.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Kita.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Mereka.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Ganti/Kata Ganti Saya.mp4" type="video/mp4">
                         </video>
                     </div>
                 </div>
@@ -41,20 +46,28 @@ include 'koneksi.php';
                     <h1>Kata Sifat</h1>
                     <div class="vid-container">
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Baik Hati.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Bohong.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Cinta.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Jahat.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Jujur.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Rindu.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Sayang.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Sifat/Kata Sifat Setia.mp4" type="video/mp4">
                         </video>
                     </div>
                 </div>
@@ -64,19 +77,15 @@ include 'koneksi.php';
                     <div class="vid-container">
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                     </div>
                 </div>
@@ -86,19 +95,15 @@ include 'koneksi.php';
                     <div class="vid-container">
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                         <video controls loop muted>
                             <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
                         </video>
                     </div>
                 </div>
@@ -107,20 +112,46 @@ include 'koneksi.php';
                     <h1>Kata Hubung</h1>
                     <div class="vid-container">
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Antara.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Atau.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Bahkan.mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Place Holder Vid.mp4" type="video/mp4">
-                            <source src="assets/vid/Place Holder Vid.webm" type="video/webm">
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Begitu.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Boleh.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Dan.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Dari.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Daripada.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Di (kata depan).mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Iya.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Karena.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Mau.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Serta.mp4" type="video/mp4">
+                        </video>
+                        <video controls loop muted>
+                            <source src="assets/vid/Kata Hubung/Kata_Hubung Tidak.mp4" type="video/mp4">
                         </video>
                     </div>
                 </div>
@@ -128,54 +159,38 @@ include 'koneksi.php';
                 <div class="materi-smallvid" id=imbuhan>
                     <h1>Imbuhan Awalan (Prefiks)</h1>
                     <div class="vid-container">
+                        <iframe src="https://www.youtube.com/embed/y_m_VQqbstQ?modestbranding=1&controls=0&mute=1" title="Imbuhan -an" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/Etd5nWYcEQU?modestbranding=1&controls=0&mute=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/r2fdakhb7L0?modestbranding=1&controls=0&mute=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/ly31XwbpdXE?modestbranding=1&controls=0&mute=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe src="https://www.youtube.com/embed/D7A_UXwMDzs?modestbranding=1&controls=0&mute=1" title="Imbuhan -lah" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
                         <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -an.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -an.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -i.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -i.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -kah.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -kah.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -kan.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -kan.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -lah.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -lah.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan -nya.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan -nya.webm" type="video/webm">
-                        </video>
-                        <h1>Imbuhan Akiran (Sufiks)</h1>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Ber-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Ber-.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Di-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Di-.webm" type="video/webm">
-                        </video>
-                        <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Me-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Me-.webm" type="video/webm">
+                            <source src="assets/vid/Imbuhan/Imbuhan (-nya).mp4" type="video/mp4">
                         </video>                        
+                    </div>
+                </div>
+                <div class="materi-smallvid">
+                    <h1>Imbuhan Akiran (Sufiks)</h1>
+                    <div class="vid-container">
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/PGC1m4jpMB0?modestbranding=1&controls=0&mute=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe width="1280" height="720" src="https://www.youtube.com/embed/E2vvN_R9ftU?modestbranding=1&controls=0&mute=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                        <iframe src="https://www.youtube.com/embed/LY66GZMjlbY?modestbranding=1&controls=0&mute=1" title="Imbuhan Me-" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
+
                         <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Pe-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Pe-.webm" type="video/webm">
+                            <source src="assets/vid/Imbuhan/Imbuhan (Pe-).mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Se-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Se-.webm" type="video/webm">
+                            <source src="assets/vid/Imbuhan/Imbuhan (Se-).mp4" type="video/mp4">
                         </video>
                         <video controls loop muted>
-                            <source src="assets/vid/Imbuhan/Imbuhan Ter-.mp4" type="video/mp4">
-                            <source src="assets/vid/Imbuhan/Imbuhan Ter-.webm" type="video/webm">
+                            <source src="assets/vid/Imbuhan/Imbuhan (Ter-).mp4" type="video/mp4">
                         </video>
                     </div>
                 </div>
